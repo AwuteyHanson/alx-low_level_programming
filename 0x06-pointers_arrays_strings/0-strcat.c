01177737464f2e66f28a0bbe5f06f31780333ce1
AwuteyHanson <sparrowhanson@gmail.com>
2022-03-25 06:47:18 -0700

    string concatenating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+trcat - appends src to the dest string
+ * @dest: string to append by src
+ *  * @src: string to append dest
+ *   *
+ *    * Return: address of dest
+ *    char *_strcat(char *dest, char *src)
+	*                  int i, j;
+	*                  	      i = j = 0;
+	*                  	      	      while (*(dest +I))
+	*                  	      	      			   i++;
+	*                  	      	      			   	       while ((*(dest + I) = *(src + j)))
+	*                  	      	      			   	       	       {
+		*                  	      	      			   	       	       			    i++;
+		*                  	      	      			   	       	       			    			    j++;
+		*                  	      	      			   	       	       			    			    	        }
+		*                  	      	      			   	       	       			    			    	        		return (dest);
+		*                  	      	      			   	       	       			    			    	        		} 
