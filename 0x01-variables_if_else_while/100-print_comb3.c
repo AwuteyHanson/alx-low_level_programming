899ef6cea2452328b427832a520219cff5384315
AwuteyHanson <sparrowhanson@gmail.com>
2022-09-16 14:42:34 -0700

    final commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int x, y;
-
+	
 	for (x = 48; x <= 57; x++)
 	{
 		for (y = 48; y <= 57; y++)
